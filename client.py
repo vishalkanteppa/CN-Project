a2a4c6e9a5132d4442eb7403d8acb745f082d7ec
vishalkanteppa <vishalkanteppa@gmail.com>
2019-11-15 11:59:37 +0530

    Additional functionalities added

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 from socket import *
 import pickle
+from getpass import getpass
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 21567
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
@@ -9,36 +10,149 @@ ADDR = (HOST, PORT)
 tcpCliSock = socket(AF_INET, SOCK_STREAM)
 tcpCliSock.connect(ADDR)
 
-while True:
+user = dict()
 
+print("TIMINGS\n")
+print("1. Jurassic Park- 10:15")
+print("2. Avengers- 12")
+print("3. The Departed- 14:30")
+print("4. Shutter Island- 16:00")
+print("5. 21 Jump Street- 19:00\n")
+
+movie = [
+    "jurassic park",
+    "avengers",
+    "the departed",
+    "shutter island",
+    "21 jump street",
+]
+
+print("Please login to continue.")
+uname = input("Username: ")
+print("This is not an existing username. Please sign up.")
+uname = input("Username: ")
+pswd = getpass("Password: ")
+print()
+user[uname] = pswd
+login = True
+
+
+def rec():
     data = tcpCliSock.recv(BUFSIZ)
-    arr=pickle.loads(data)
-    a=pickle.loads(arr[0])
-    check=arr[1]
+    arr = pickle.loads(data)
+    a = pickle.loads(arr[0])
+    check = arr[1]
+    return (a, check)
 
-    if check==True:
-        for i in a:
-            for j in i:
-                print(j,end='    ')
-            print()
-        print()
+
+def sen(s, a, name):
+
+    seat = int(s)
+    if seat > 50:
+        print("Please enter a valid seat number.\n")
+        return 0
+
+    if seat != -1:
+        row = int(seat / 10)
+        col = (seat % 10) - 1
     else:
-        print("Seat already taken. Please try again.\n")  
-    
-    s=input('> Enter seat you wish to sit in: ')
-    if not s:
-        break
-    seat=int(s)
-
-    row=int(seat/10)
-    col=(seat%10)-1
-    
-    a=pickle.dumps(a)
-    arr=[a,row,col]
+        row = -1
+        col = -1
+    a = pickle.dumps(a)
+    arr = [a, row, col, name]
     # print(arr)
     tcpCliSock.send(pickle.dumps(arr))
-    
-    # print(pickle.load(data))
-    # print(pickle.loads(data))
+    return 1
+
+t=1
+decide = -1
+a, check = rec()
+name = "avengers"
+while True:
+    if login:
+        decide *= -1
+
+        name = "avengers"
+        sen(-1, a, name)
+        a, check = rec()
+
+        if t==1:
+            print('Type "logout" to logout and "exit" to exit.\n')
+            t=0
+
+        if decide == 1:
+            name = input("Enter movie name. \n")
+
+            if name.lower() == "logout":
+                login = False
+                continue
+            elif name.lower() == "exit":
+                sen(-1, a, name)
+                break
+
+            if name.lower() not in movie:
+                print(
+                    "Sorry that movie does not seem to be playing today. Please select one from the list.\n"
+                )
+                continue
+
+            if check == True:
+                for i in a[name]:
+                    for j in i:
+                        print(j, end="    ")
+                    print()
+                print()
+            s = input("Enter seat you wish to sit in. ")
+
+            flag = sen(s, a, name)
+            if flag:
+                a, check = rec()
+            if check == True and flag:
+                print("Booking done succesfully!\n")
+
+            while check == False:
+                print(
+                    "Sorry that does not seem to be a valid seat. Please try again.\n"
+                )
+                s = input("Enter seat you wish to sit in. ")
+                flag = sen(s, a, name)
+                if flag:
+                    a, check = rec()
+                if check == True and flag:
+                    print("Booking done succesfully!\n")
+
+        elif decide == -1:
+            while name.lower() not in movie:
+                print(
+                    "Sorry that movie does not seem to be playing today. Please select one from the list."
+                )
+                name = input("Enter MMovie name. \n")
+
+    elif not login:
+        print("You seem to be signed out. Please login to continue.")
+        while not login:
+            uname = input("Username: ")
+            if uname not in user:
+                print("User does not exist. Please sign up.")
+                uname = input("Username: ")
+                pswd = getpass("Paswword: ")
+                user[uname] = pswd
+                login = True
+            elif uname in user:
+                pswd = getpass("Paswword: ")
+                if user[uname] == pswd:
+                    print("Login Successful!")
+                    login = True
+                    continue
+                else:
+                    print("User already exists. \n")
+            else:
+                pswd = getpass("Password: ")
+                if user[uname] == pswd:
+                    print("Login Successful!\n")
+                    login = True
+                    continue
+                else:
+                    print("Incorrect Password.")
 
 tcpCliSock.close()
--- server.py
+++ server.py
@@ -1,39 +1,51 @@
-
 from socket import *
 from time import ctime
 import pickle
 
-HOST = ''
+HOST = ""
 PORT = 21567
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 
-a = [[0 for i in range(10)] for j in range(5)] 
+a = dict()
+a["jurassic park"] = [[0 for i in range(10)] for j in range(5)]
+a["avengers"] = [[0 for i in range(10)] for j in range(5)]
+a["21 jump street"] = [[0 for i in range(10)] for j in range(5)]
+a["the departed"] = [[0 for i in range(10)] for j in range(5)]
+a["shutter island"] = [[0 for i in range(10)] for j in range(5)]
 
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
 tcpCliSock, addr = tcpSerSock.accept()
+print("Connection established to:",addr,"\n")
 
-check=True
+check = True
+i = 0
 while True:
-
-    arr=[pickle.dumps(a),check]
+    arr = [pickle.dumps(a), check]
     tcpCliSock.send(pickle.dumps(arr))
+    i += 1
+    # print(i)
 
-    data=tcpCliSock.recv(BUFSIZ)
-    arr=pickle.loads(data)
+    data = tcpCliSock.recv(BUFSIZ)
+    arr = pickle.loads(data)
     if not data:
         break
-    a=pickle.loads(arr[0])
-    r=arr[1]
-    c=arr[2]
+    a = pickle.loads(arr[0])
+    r = arr[1]
+    c = arr[2]
+    n = arr[3]
 
-    if a[r][c]==1:
-        check=False
-    else:
-        a[r][c]=1
-        check=True
+    if n.lower() == "exit":
+        break
+
+    if a[n][r][c] == 1 and r != -1:
+        check = False
+    elif a[n][r][c] == 0 and r != -1:
+        a[n][r][c] = 1
+        check = True
 
 tcpCliSock.close()
 tcpSerSock.close()
+
